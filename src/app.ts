5e8118f1f4e49225286ab7ad54523828a4e71868
Vasanth Gouravarapu <vasanth.gouravarapu@assettl.com>
2026-08-23 19:44:54 +0530

    Route Handlers and middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,21 @@
-import express from 'express'
+import express, { response } from 'express'
 
 const app = express()
-const port = 445
+const port = 4200
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 })
+
+app.use("/test", (req, res, next) => {
+    console.log("Testing the route handler function")
+    next()
+},
+    /*The block of code called as route handlers */
+    (req, res) => {
+        res.send("Testing the next() function")
+    }
+)
 
 app.get("/home", (req, res) => {
     res.send("Hi this is my new project")
