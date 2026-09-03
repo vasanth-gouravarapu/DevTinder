350d28aeb5008a19210dae943aeb23a49e9aaa61
Vasanth Gouravarapu <vasanth.gouravarapu@assettl.com>
2026-09-03 15:40:15 +0530

    test

---
+++ config.ts
@@ -0,0 +1,3 @@
+const URLS ={
+    Mongoo_connection : "mongodb+srv://gouravarapuvasanth_db_user:Dyq8Ya9TjNJOpOCv@cluster0.mtpnls3.mongodb.net/?appName=Cluster0"  
+}
--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,10 @@ app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 })
 
+app.get("/home", (req, res) => {
+    res.send("Hi this is my new project")
+})
+
 app.use("/test", (req, res, next) => {
     console.log("Testing the route handler function")
     next()
@@ -17,6 +21,4 @@ app.use("/test", (req, res, next) => {
     }
 )
 
-app.get("/home", (req, res) => {
-    res.send("Hi this is my new project")
-})
+
